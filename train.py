5eb0b27f2aabb52375f38c920cc2fa250ffd7dc0
Junho Han <jyuno426@kaist.ac.kr>
2020-03-11 16:43:46 +0900

    .

--- train.py
+++ train.py
@@ -5,15 +5,31 @@ import tqdm
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from utility import EarlyStopping
+from utils import EarlyStopping
 from torch.utils.data import DataLoader
 from reformer_pytorch import ReformerLM
+
+
+def save(path, data):
+    with open(path, "a+") as f:
+        f.write(str(data) + "\n")
 
 
 def cycle(loader):
     while True:
         for data in loader:
             yield data
+
+
+def accuracy(output, target):
+    _output = output.view(-1, output.shape[-1])
+    _target = target.view(-1)
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
+    return correct / len(target)
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -30,7 +46,7 @@ optimizer = torch.optim.Adam
 
 # constants not revealed in the paper
 emb_dim = 512
-batch_num = 5000 # 4 epochs for 160000 training sets
+batch_num = 5000  # 4 epochs for 160000 training sets
 
 
 in_data = []
@@ -83,10 +99,11 @@ train_data = dataset[: int(data_len * 0.8)]
 valid_data = dataset[int(data_len * 0.8) : int(data_len * 0.9)]
 test_data = dataset[int(data_len * 0.9) :]
 
-train_loader = cycle(DataLoader(train_data, batch_size=int(batch_size), shuffle=True))
+train_loader = cycle(DataLoader(train_data, batch_size=batch_size, shuffle=True))
 valid_loader = cycle(DataLoader(valid_data, batch_size=batch_size, shuffle=True))
 
 print("Building model ....")
+
 
 class MyModel(torch.nn.Module):
     def __init__(self):
@@ -100,10 +117,10 @@ class MyModel(torch.nn.Module):
             dim=dim,
             fixed_position_emb=True,
             return_embeddings=True,
-            lsh_dropout = 0.1,
-            ff_dropout = 0.1,
-            post_attn_dropout = 0.1,
-            layer_dropout = 0.1,
+            lsh_dropout=0.1,
+            ff_dropout=0.1,
+            post_attn_dropout=0.1,
+            layer_dropout=0.1,
         ).to(device)
 
         self.decoder = ReformerLM(
@@ -115,10 +132,10 @@ class MyModel(torch.nn.Module):
             dim=dim,
             fixed_position_emb=True,
             causal=True,
-            lsh_dropout = 0.1,
-            ff_dropout = 0.1,
-            post_attn_dropout = 0.1,
-            layer_dropout = 0.1,
+            lsh_dropout=0.1,
+            ff_dropout=0.1,
+            post_attn_dropout=0.1,
+            layer_dropout=0.1,
         ).to(device)
 
     def forward(self, source, target):
@@ -134,54 +151,60 @@ loss_ftn = torch.nn.CrossEntropyLoss(ignore_index=0, reduction="sum").to(device)
 
 print("Start training!")
 
-early_stopping = EarlyStopping(patience=100, verbose=True)
+early_stopping = EarlyStopping(patience=10, verbose=True)
 
 torch.cuda.empty_cache()
 
-train_losses = []
-valid_losses = []
+# train_losses = []
+# valid_losses = []
+# train_acc = []
+# valid_acc = []
 for i in tqdm.tqdm(range(batch_num), mininterval=10, desc="training"):
 
     model.train()
 
-    for _ in range(1):
-        batch = next(train_loader).to(device)
-        output = model(batch[:, 0, :], batch[:, 1, :])
-        loss = loss_ftn(output.contiguous().view(-1, len(token_dict)), batch[:, 1, :].contiguous().view(-1))
-        loss = loss.mean()
-        loss.backward()
+    batch = next(train_loader).to(device)
+    output = model(batch[:, 0, :], batch[:, 1, :])
+    loss = loss_ftn(
+        output.contiguous().view(-1, len(token_dict)),
+        batch[:, 1, :].contiguous().view(-1),
+    )
+    loss = loss.mean()
+    loss.backward()
 
     loss_value = loss.item()
     print(f"training loss: {loss_value}")
-    train_losses.append(loss_value)
+    save("train_loss", loss_value)
+    save("train_acc", round(accuracy(output, batch[:, 1, :]) * 100, 5))
 
     torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
     optim.step()
     optim.zero_grad()
 
-    if i % 4 == 0:
-        model.eval()
-        with torch.no_grad():
-            batch = next(valid_loader).to(device)
-            output = model(batch[:, 0, :], batch[:, 1, :])
-            loss = loss_ftn(output.contiguous().view(-1, len(token_dict)), batch[:, 1, :].contiguous().view(-1))
-            loss = loss.mean()
+    model.eval()
+    with torch.no_grad():
+        batch = next(valid_loader).to(device)
+        output = model(batch[:, 0, :], batch[:, 1, :])
+        loss = loss_ftn(
+            output.contiguous().view(-1, len(token_dict)),
+            batch[:, 1, :].contiguous().view(-1),
+        )
+        loss = loss.mean()
 
-            loss_value = loss.item()
-            print(f"valid loss: {loss_value}")
-            valid_losses.append(loss_value)
+        loss_value = loss.item()
+        print(f"valid loss: {loss_value}")
+        save("valid_loss", loss_value)
+        save("valid_acc", round(accuracy(output, batch[:, 1, :]) * 100, 5))
 
-            # early_stopping needs the validation loss to check if it has decresed,
-            # and if it has, it will make a checkpoint of the current model
-            early_stopping(loss_value, model)
+        # early_stopping needs the validation loss to check if it has decresed,
+        # and if it has, it will make a checkpoint of the current model
+        early_stopping(loss_value, model)
 
-            if early_stopping.early_stop:
-                print("Early stopping")
-                break
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
 
     torch.cuda.empty_cache()
 
-json.dump(train_losses, open("train_losses.json", "w"))
-json.dump(valid_losses, open("valid_losses.json", "w"))
 # load the last checkpoint with the best model
 # model.load_state_dict(torch.load("checkpoint.pt"))
