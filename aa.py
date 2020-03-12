d19f60195dbb92de0fbbede732fee6fe800f1232
Junho Han <jyuno426@kaist.ac.kr>
2020-03-13 00:40:41 +0900

    .

--- aa.py
+++ aa.py
@@ -1,6 +1,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 f1 = open("./dataset/local/src-train.txt", "w")
 f2 = open("./dataset/local/src-val.txt", "w")
@@ -9,54 +10,32 @@ f4 = open("./dataset/local/tgt-train.txt", "w")
 f5 = open("./dataset/local/tgt-val.txt", "w")
 f6 = open("./dataset/local/tgt-test.txt", "w")
 
-# mappp = {
-#     "0": "zero",
-#     "1": "one",
-#     "2": "two",
-#     "3": "three",
-#     "4": "four",
-#     "5": "five",
-#     "6": "six",
-#     "7": "seven",
-#     "8": "eight",
-#     "9": "nine",
-#     ",": " ",
-# }
-
-
-# def mapp(string):
-#     string = string.strip()
-#     for key, val in mappp.items():
-#         string = string.replace(key, val)
-#     return string
-
-
-# def unmapp(string):
-#     string = string.strip()
-#     for key, val in mappp.items():
-#         string = string.replace(val, key)
-#     return string
-
-
 with open("./dataset/local/integration.in", "r") as f:
-    lines = f.readlines()
-    lines = lines[np.random.permutation(len(lines))]
-    for i in range(len(lines) * 0.8):
-        f1.write(f.readline().replace(",", " "))
-    for i in range(len(lines) * 0.1):
-        f2.write(f.readline().replace(",", " "))
-    for i in range(len(lines) * 0.1):
-        f3.write(f.readline().replace(",", " "))
+    with open("./dataset/local/integration.in", "r") as g:
+        in_lines = f.readlines()
+        out_lines = g.readlines()
+        assert len(in_lines) == len(out_lines) and len(in_lines) == 220000
+        random_indexes = np.random.permutation(len(in_lines))
 
-with open("./dataset/local/integration.out", "r") as f:
-    lines = f.readlines()
-    lines = lines[np.random.permutation(len(lines))]
-    for i in range(len(lines) * 0.8):
-        f4.write(f.readline().replace(",", " "))
-    for i in range(len(lines) * 0.1):
-        f5.write(f.readline().replace(",", " "))
-    for i in range(len(lines) * 0.1):
-        f6.write(f.readline().replace(",", " "))
+        i = 0
+        while i < 210000:
+            f1.write(in_lines[random_indexes[i]].strip().replace(",", " ") +
+                     '\n')
+            f4.write(out_lines[random_indexes[i]].strip().replace(",", " ") +
+                     '\n')
+            i += 1
+        while i < 215000:
+            f2.write(in_lines[random_indexes[i]].strip().replace(",", " ") +
+                     '\n')
+            f5.write(out_lines[random_indexes[i]].strip().replace(",", " ") +
+                     '\n')
+            i += 1
+        while i < 220000:
+            f3.write(in_lines[random_indexes[i]].strip().replace(",", " ") +
+                     '\n')
+            f6.write(out_lines[random_indexes[i]].strip().replace(",", " ") +
+                     '\n')
+            i += 1
 
 import sys
 
@@ -81,8 +60,8 @@ with open("valid_acc", "r") as f:
 batch_size = 128
 x1 = np.array(range(batch_size, len(train_loss) * batch_size + 1, batch_size))
 x2 = np.array(
-    range(8 * batch_size, 8 * batch_size * len(valid_loss) + 1, 8 * batch_size)
-)
+    range(8 * batch_size, 8 * batch_size * len(valid_loss) + 1,
+          8 * batch_size))
 
 plt.title("Integration")
 plt.plot(x1, train_loss, label="train_loss")
